f2801eb274650c2e00107d12732422d43e6bb7cf
vvauban <vvauban@gmail.com>
2022-12-09 10:15:10 +0100

    Typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import PaymentCardInput from './PaymentCardinput';
+import PaymentCardInput from './PaymentCardInput';
 import PhoneNumberInput from './PhoneNumberInput';
 
 function App() {
